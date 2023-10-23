e86f330733a65c06d5f2428f30a52fb2ff89ee65
Nerzul <nerkur@bk.ru>
2023-10-23 19:17:11 +0600

    btn

--- apps/org/src/app/app.component.ts
+++ apps/org/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { UiModule } from '@org/ui';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule,UiModule],
   selector: 'org-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
