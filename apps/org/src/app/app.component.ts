a47eee336dff5bd7b9b14f225e56f7cee5614067
Nerzul <nerkur@bk.ru>
2023-10-24 00:40:29 +0600

    asd

--- apps/org/src/app/app.component.ts
+++ apps/org/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { UiModule } from '@org/ui';
+import { SharedModule } from 'asdggsdlib';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule,UiModule],
+  imports: [NxWelcomeComponent, RouterModule,UiModule,SharedModule],
   selector: 'org-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
