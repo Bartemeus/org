7bc4232a85264d14cd5c7d47c3eef1c3cecc671b
Nerzul <nerkur@bk.ru>
2023-11-19 19:21:11 +0600

    asd

--- apps/org/src/app/app.component.ts
+++ apps/org/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { UiModule } from '@org/ui';
 import { SharedModule } from 'asdggsdlib';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule,UiModule,SharedModule],
+  imports: [NxWelcomeComponent, RouterModule,SharedModule],
   selector: 'org-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
